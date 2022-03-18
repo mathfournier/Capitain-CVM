bcee7437d98843a07341ff886d5e6cecbba76ae5
mathfournier <mathsoc@hotmail.com>
2022-03-17 23:57:52 -0400

    lvl2 almost done manque pancarte

---
+++ Assets/Scripts/Ennemy/BatBehaviour.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BatBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// Point de vie du personnage
+    /// </summary>
+    [SerializeField]
+    private int _pv = 2;
+    /// <summary>
+    /// Angle de tolérange pour le calcul du saut sur la tête
+    /// </summary>
+    [SerializeField]
+    private float _toleranceAngle = 1.5f;
+    /// <summary>
+    /// Décrit la durée de l'invulnaribilité
+    /// </summary>
+    public const float DelaisInvulnerabilite = 1f;
+    /// <summary>
+    /// Décrit si l'entité est invulnérable
+    /// </summary>
+    private bool _invulnerable = false;
+    /// <summary>
+    /// Réfère à l'animator du GO
+    /// </summary>
+    private Animator _animator;
+    /// <summary>
+    /// Représente le moment où l'invulnaribilité a commencé
+    /// </summary>
+    private float _tempsDebutInvulnerabilite;
+    /// <summary>
+    /// Nombre de points octroyer lors de la destruction
+    /// </summary>
+    [SerializeField]
+    private int _pointDestruction = 5;
+    /// <summary>
+    /// Défini si l'objet est en cours de destruction
+    /// </summary>
+    private bool _destructionEnCours = false;
+    /// <summary>
+    /// Défini si l'objet est down et prêt a etre attaqué
+    /// </summary>
+    private bool _estDowned = false;
+    private float _timeDowned = 5f;
+    private float _timeEntreAttaque = 2.5f;
+    private float _tempsDebutDowned;
+    private bool _wasAttacked = false;
+    private bool _canAttack = false;
+
+    private void Start()
+    {
+        _animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (this._pv <= 0 && !this._destructionEnCours)
+        {
+            _animator.SetTrigger("Destruction");
+            GameManager.Instance.PlayerData.IncrScore(this._pointDestruction);
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            this.gameObject.GetComponent<EnnemyPatrol>().enabled = false;
+            GameObject.Destroy(this.transform.parent.gameObject, 0.5f);
+            this._destructionEnCours = true;
+        }
+
+        if (Time.fixedTime > _tempsDebutInvulnerabilite + DelaisInvulnerabilite)
+            _invulnerable = false;
+
+        if (Time.fixedTime > _tempsDebutDowned + _timeEntreAttaque)
+            _canAttack = true;
+
+        if (Time.fixedTime > _tempsDebutDowned + _timeDowned)
+        {
+            _estDowned = false;
+            if(!_wasAttacked)
+                _animator.SetTrigger("NotAttacked");
+            _wasAttacked = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player") && !_invulnerable)
+        {
+            PlayerBehaviour pb = collision.gameObject.GetComponent<PlayerBehaviour>();
+            if (pb != null)
+                pb.CallEnnemyCollision();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            if(!_invulnerable)
+            {
+                if(_estDowned && _canAttack)
+                {
+                    this._pv--;
+                    _animator.SetTrigger("DegatActif");
+                    _tempsDebutInvulnerabilite = Time.fixedTime;
+                    _invulnerable = true;
+                    _estDowned = false;
+                    _wasAttacked = true;
+                    _timeDowned = 0;
+                    _canAttack = false;
+                }
+                else
+                {
+                    _animator.SetTrigger("Downed");
+                    _tempsDebutDowned = Time.fixedTime;
+                    _estDowned = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Ennemy/BatPatrol.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BatPatrol : MonoBehaviour
+{
+    /// <summary>
+    /// Vitesse de l'objet en patrouille
+    /// </summary>
+    [SerializeField]
+    private float _vitesse = 3f;
+    /// <summary>
+    /// Liste de GO représentant les points à atteindre
+    /// </summary>
+    [SerializeField]
+    private Transform[] _points;
+    /// <summary>
+    /// Référence vers la cible actuelle de l'objet
+    /// </summary>
+    private Transform _cible = null;
+    /// <summary>
+    /// Permet de connaître la position actuelle de la cible dans le tableau
+    /// </summary>
+    private int _indexPoint;
+    /// <summary>
+    /// Seuil où l'objet change de cible de déplacement
+    /// </summary>
+    private float _distanceSeuil = 0.3f;
+    /// <summary>
+    /// Référence vers le sprite Renderer
+    /// </summary>
+    private SpriteRenderer _sr;
+    /// <summary>
+    /// Réfère à l'animator du GO
+    /// </summary>
+    private Animator _animator;
+    private bool _estDowned = false;
+    private int _tempsDowned = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sr = this.GetComponent<SpriteRenderer>();
+        _indexPoint = 0;
+        _cible = _points[_indexPoint];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_estDowned)   //Tentative de management pour l'arrêt de la chauve-souris durant le moment quelle est downed. 
+        {
+            _tempsDowned++;
+            if (_tempsDowned > 100)
+            {
+                _estDowned = false;
+            }
+        }
+        AnimatorStateInfo asi = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);  //Ne fonctionne pas. Alternative non plus
+        if (asi.IsName("Bat_Down"))
+        {
+            Debug.Log("EST DOWNED");
+            _estDowned = true;
+            _tempsDowned = 0;
+        }
+        
+        if(!_estDowned)
+        {
+            Vector3 direction = _cible.position - this.transform.position;
+            this.transform.Translate(direction.normalized * _vitesse * Time.deltaTime, Space.World);
+
+            if (direction.x < 0 && !_sr.flipX) _sr.flipX = true;
+            else if (direction.x > 0 && _sr.flipX) _sr.flipX = false;
+
+            if (Vector3.Distance(this.transform.position, _cible.position) < _distanceSeuil)
+            {
+                _indexPoint = (++_indexPoint) % _points.Length;
+                _cible = _points[_indexPoint];
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        // Ligne entre les cibles
+        for (int i = 0; i < _points.Length - 1; i++)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(_points[i].position, 
+                _points[i + 1].position);
+        }
+
+        // Ligne entre l'ennemi et la cible
+        if (_cible != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(this.transform.position, _cible.position);
+        }
+    }
+#endif
+}
